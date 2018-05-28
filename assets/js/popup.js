dd0d87fe3163b9a3e62c587a059928082295e6fd
Jaime Hernández <jaimevazz@gmail.com>
2018-05-28 13:20:18 -0500

    Demo Reel button and try for modal scroll disabling

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -19,12 +19,12 @@ var popup = (function(){
   var _update = function (state) {
     _play_anim(state);
     _set_text(state);
-    // if (state == STATES.Show) {
-    //   $("body").addClass("modal-open");
-    // }
-    // else if (state == STATES.Hide) {
-    //   $("body").removeClass("modal-open");
-    // }
+    if (state == STATES.Show) {
+      $("body").addClass("modal-open");
+    }
+    else if (state == STATES.Hide) {
+      $("body").removeClass("modal-open");
+    }
 
   };
   // Public methods
